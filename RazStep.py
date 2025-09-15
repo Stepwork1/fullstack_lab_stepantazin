46fec96e844c0fd6d1764d7828503ae7c49b54c9
StepanRazin <DZ.StepanRazin@yandex.com>
2025-09-16 00:19:15 +0500

    Update RazStep.py
    iimrt zadal vam trend, kajdui znaet eto nrend

--- RazStep.py
+++ RazStep.py
@@ -2,6 +2,7 @@ def main():
     print('Razin Stepan')
     print('ИВТ-ИСУ-202Б, 1 подгруппа')
     print('15.09.2025')
+    print('Vrag moy boisya menya, drug moy ne otvergaisya ot menya')
 
 
 if __name__ == '__main__':
